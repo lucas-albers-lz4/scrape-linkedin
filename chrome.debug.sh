334cafdd6881d2b3a45ff3857fc0069e432c8965
Lucas Albers <lucas.b.albers@gmail.com>
2025-04-11 10:42:19 -0600

    revise script to start chrome in debug mode (selenium needs this to connect)

--- chrome.debug.sh
+++ chrome.debug.sh
@@ -1,1 +1,2 @@
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-infobars --remote-debugging-port=9222 --log-level=3 --silent --disable-logging --disable-features=BlockInsecurePrivateNetworkRequests
+#!/bin/sh
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-infobars --remote-debugging-port=9222 --log-level=3 --silent --disable-logging --disable-features=BlockInsecurePrivateNetworkRequests > /dev/null 2> /dev/null
